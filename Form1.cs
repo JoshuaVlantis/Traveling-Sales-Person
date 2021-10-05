7f04202f75640dedfba9f758417879e23b7c05e2
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2021-10-05 22:29:44 +0200

    Getting there only half broken

--- Form1.cs
+++ Form1.cs
@@ -95,34 +95,44 @@ namespace Traveling_Sales_Person
         }
         
         //Draws lines on BMP
-
         public void drawfinal()
         {
             Graphics g = Graphics.FromImage(bmp);
 
-            Redrawdrawdot();
-
             int x1;
             int y1;
             int x2;
-            int y2;  
+            int y2;
 
             Random rnd = new Random();
             Pen blackPen = new Pen(Color.White, 2);
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 10; i++)
             {
-                x1 = pos[i, 0];
-                y1 = pos[i, 1];
-                x2 = pos[i+1, 0];
-                y2 = pos[i+1, 1];
-                                
+                if (i != 9)
+                {
+                    x1 = shortestpos[i, 0];
+                    y1 = shortestpos[i, 1];
+                    x2 = shortestpos[i + 1, 0];
+                    y2 = shortestpos[i + 1, 1];
+                }
+                else
+                {
+                    x1 = shortestpos[0, 0];
+                    y1 = shortestpos[0, 1];
+                    x2 = shortestpos[9, 0];
+                    y2 = shortestpos[9, 1];
+                }
+
                 // Draw line to screen.
                 using (var graphics = Graphics.FromImage(bmp))
                 {
                     graphics.DrawLine(blackPen, x1, y1, x2, y2);
                 }
-            }  
+                Thread2();
+
+                Thread.Sleep(10);
+            }
         }
         public void Drawfirst()
         {
@@ -163,9 +173,86 @@ namespace Traveling_Sales_Person
                 Thread.Sleep(10);
             }
         }
+
+        public bool alreadyused(int i)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                if (pos[i,0] == shortestpos[x,0] && pos[i, 1] == shortestpos[x, 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void workoutpoints()
+        {
+            int i = 0;
+            int ii;
+
+            int xaxis;
+            int yaxis;
+
+            int x1;
+            int y1;
+            int x2;
+            int y2;
+
+            Double hypotenuse = 0;
+            Double smallesthypotenuse = 0;
+
+            for (; i < 10; i++)
+            {
+                ii = 0;
+                x1 = pos[i, 0];
+                y1 = pos[i, 1];
+                for (; ii < 10; ii++)
+                {
+                    x2 = pos[ii, 0];
+                    y2 = pos[ii, 1];
+
+                    if (x1 > x2)
+                    {
+                        xaxis = x1 - x2;
+                    }
+                    else
+                    {
+                        xaxis = x2 - x1;
+                    }
+                    // Y Gets distance between points
+                    if (y1 > y2)
+                    {
+                        yaxis = y1 - y2;
+                    }
+                    else
+                    {
+                        yaxis = y2 - y1;
+                    }
+
+                    hypotenuse = (xaxis * xaxis) + (yaxis * yaxis);
+                    hypotenuse = (Math.Sqrt(hypotenuse));
+
+                    if (hypotenuse <= smallesthypotenuse || i == 0)
+                    {
+                        if (!alreadyused(i))
+                        {
+                            smallesthypotenuse = hypotenuse;
+
+                            shortestpos[i, 0] = x2;
+                            shortestpos[i, 1] = y2;
+                        }
+                    }
+                }
+
+            }
+            drawfinal();
+        }
+
         public void Thread1()
         {
-            Drawfirst();
+            //Drawfirst();
+            workoutpoints();
         }
 
         public void Thread2()

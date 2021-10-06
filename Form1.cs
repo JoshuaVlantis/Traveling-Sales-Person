997fb82b333543edfb16391f5e285bc3912ab6bf
Joshua Vlantis <70960513+JoshuaVlantis@users.noreply.github.com>
2021-10-06 19:53:50 +0200

    REWRITING AGAIN!!!!!!!!!!!

--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,7 @@ namespace Traveling_Sales_Person
         int[,] pos = new int[10,2];            //Cords are stored in a 2d array (X cord on left, Y cord on right)
         int[,] shortestpos = new int[10,2];    //These cords will be used to stored the shortest path
         bool haspoints = false;
+        int iloop = 0;
 
         public Form1()
         {
@@ -94,46 +95,6 @@ namespace Traveling_Sales_Person
             image.Image = bmp;
         }
         
-        //Draws lines on BMP
-        public void drawfinal()
-        {
-            Graphics g = Graphics.FromImage(bmp);
-
-            int x1;
-            int y1;
-            int x2;
-            int y2;
-
-            Random rnd = new Random();
-            Pen blackPen = new Pen(Color.White, 2);
-
-            for (int i = 0; i < 10; i++)
-            {
-                if (i != 9)
-                {
-                    x1 = shortestpos[i, 0];
-                    y1 = shortestpos[i, 1];
-                    x2 = shortestpos[i + 1, 0];
-                    y2 = shortestpos[i + 1, 1];
-                }
-                else
-                {
-                    x1 = shortestpos[0, 0];
-                    y1 = shortestpos[0, 1];
-                    x2 = shortestpos[9, 0];
-                    y2 = shortestpos[9, 1];
-                }
-
-                // Draw line to screen.
-                using (var graphics = Graphics.FromImage(bmp))
-                {
-                    graphics.DrawLine(blackPen, x1, y1, x2, y2);
-                }
-                Thread2();
-
-                Thread.Sleep(10);
-            }
-        }
         public void Drawfirst()
         {
             Graphics g = Graphics.FromImage(bmp);
@@ -174,44 +135,85 @@ namespace Traveling_Sales_Person
             }
         }
 
-        public bool alreadyused(int i)
+        public bool alreadyused()
         {
             for (int x = 0; x < 10; x++)
             {
-                if (pos[i,0] == shortestpos[x,0] && pos[i, 1] == shortestpos[x, 1])
+                if (pos[iloop,0] == shortestpos[x,0] && pos[iloop, 1] == shortestpos[x, 1])
                 {
                     return true;
                 }
             }
             return false;
         }
-
-        public void workoutpoints()
+        public void drawfinal()
         {
-            int i = 0;
-            int ii;
-
-            int xaxis;
-            int yaxis;
+            Graphics g = Graphics.FromImage(bmp);
 
             int x1;
             int y1;
             int x2;
             int y2;
 
-            Double hypotenuse = 0;
-            Double smallesthypotenuse = 0;
+            Random rnd = new Random();
+            Pen blackPen = new Pen(Color.White, 2);
 
-            for (; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                ii = 0;
-                x1 = pos[i, 0];
-                y1 = pos[i, 1];
-                for (; ii < 10; ii++)
+                if (i != 9)
                 {
-                    x2 = pos[ii, 0];
-                    y2 = pos[ii, 1];
+                    x1 = shortestpos[i, 0];
+                    y1 = shortestpos[i, 1];
+                    x2 = shortestpos[i + 1, 0];
+                    y2 = shortestpos[i + 1, 1];
+                }
+                else
+                {
+                    x1 = shortestpos[0, 0];
+                    y1 = shortestpos[0, 1];
+                    x2 = shortestpos[9, 0];
+                    y2 = shortestpos[9, 1];
+                }
 
+                // Draw line to screen.
+                if (x1 + y1 + x2 + y2 != 0)
+                {
+                    using (var graphics = Graphics.FromImage(bmp))
+                    {
+                        graphics.DrawLine(blackPen, x1, y1, x2, y2);
+                    }
+                }
+
+                Thread2();
+
+                Thread.Sleep(10);
+            }
+        }
+        public void workoutpoints()
+        {
+            int x1 = 0;
+            int y1 = 0;
+            int x2 = 0;
+            int y2 = 0;
+            
+            int xaxis;
+            int yaxis;
+
+            double hypotenuse = 0;
+            double smallesthypotenuse = 0;
+
+            //Generates the first set of cords to be compared with all other cords
+            for (iloop = 0; iloop < 10; iloop++)
+            {
+                x1 = iloop;
+                y1 = iloop;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    x2 = i;
+                    y2 = i;
+
+                    //X Gets distance between points
                     if (x1 > x2)
                     {
                         xaxis = x1 - x2;
@@ -220,7 +222,7 @@ namespace Traveling_Sales_Person
                     {
                         xaxis = x2 - x1;
                     }
-                    // Y Gets distance between points
+                    //Y Gets distance between points
                     if (y1 > y2)
                     {
                         yaxis = y1 - y2;
@@ -233,20 +235,19 @@ namespace Traveling_Sales_Person
                     hypotenuse = (xaxis * xaxis) + (yaxis * yaxis);
                     hypotenuse = (Math.Sqrt(hypotenuse));
 
-                    if (hypotenuse <= smallesthypotenuse || i == 0)
+                    if (hypotenuse <= smallesthypotenuse || iloop == 0)
                     {
-                        if (!alreadyused(i))
+                        if (!alreadyused())
                         {
                             smallesthypotenuse = hypotenuse;
 
-                            shortestpos[i, 0] = x2;
-                            shortestpos[i, 1] = y2;
+                            shortestpos[iloop, 0] = x2;
+                            shortestpos[iloop, 1] = y2;
                         }
                     }
                 }
-
             }
-            drawfinal();
+
         }
 
         public void Thread1()
